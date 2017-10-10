4947e6fd52ad39cc0a07640ff39fbb23f29cb7e8
Brillante Wang <brillante.wang@gmail.com>
2017-10-10 13:42:13 -0700

    Add Steppable Pieces: Knight, King Piece

--- piece.rb
+++ piece.rb
@@ -1,13 +1,57 @@
+require_relative "stepable"
+
 class Piece
   attr_reader :type
   attr_accessor :pos
 
-  def initialize(type, pos)
-    @type = type
+  def initialize(pos)
     @pos = pos
   end
+
 end
 
+class King < Piece
+  include Stepable
+
+  protected
+
+  def move_diffs
+    [[-1, 0], [1, 0], [0, 1], [0, -1]]
+  end
+
+end
+
+class Knight < Piece
+  include Stepable
+
+  protected
+
+  def move_diffs
+    [[-2, -1], [-2, 1], [2, 1], [2, -1], [-1, -2], [1, -2], [1, 2], [-1, 2]]
+  end
+
+end
+
+
+
 class NullPiece < Piece
+
+end
+
+
+# class Knight < Piece
+#   include "stepable"
+#
+#   attr_reader :move_diffs
+#
+#   def initialize(pos)
+#     @move_diffs = [[-2, -1], [-2, 1], [2, 1], [2, -1], [-1, -2], [1, -2], [1, 2], [-1, 2]]
+#     super
+#   end
+# end
+
+if __FILE__ == $PROGRAM_NAME
+  king = King.new([0,0])
+  p king.moves
 
 end
